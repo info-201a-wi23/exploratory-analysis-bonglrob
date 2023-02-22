2d06d60cf7aaf71484444138e8d91a97c3067410
evelynsm <emijan@uw.edu>
2023-02-21 21:52:53 -0800

    update new chart 3

---
+++ chartt3.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("~/Downloads/NCHS_-_Leading_Causes_of_Death__United_States.csv")
+
+bar_data <- data[data$Year == 2017,]
+newbar_data <- bar_data[bar_data$`Cause.Name` != "All causes",]
+bardata_new <- newbar_data[newbar_data$State != "United States",]
+ 
+ggplot(bardata_new,aes(x=`Cause Name`, y= Deaths, fill= `Cause Name`)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Top 10 Leading Causes of Deaths in the US in 2017" ,
+       x = "Cause of Death",
+       y = "Number of Deaths") +
+  scale_y_continuous(labels = function(x) format(x, scientific = FALSE))+
+  theme(axis.text.x = element_text(size = 6, angle = 0, hjust = .5))
