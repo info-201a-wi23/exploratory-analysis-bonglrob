b22a65ccbd76018d3188dad745833b72c65c5bd2
Robert Bonglamphone <bonglrob@uw.edu>
2023-02-21 22:49:38 -0800

    fix chart3

--- chart3.R
+++ chart3.R
@@ -1,13 +1,12 @@
 library(ggplot2)
 
-data <- read_csv("~/Downloads/NCHS_-_Leading_Causes_of_Death__United_States.csv")
- view(data)
+data <- read.csv("us-deaths.csv")
  
  bar_data <- data[data$Year == 2017,]
  newbar_data <- bar_data[bar_data$`Cause.Name` != "All causes",]
  bardata_new <- newbar_data[newbar_data$State != "United States",]
  
-ggplot(bardata_new,aes(x=`Cause Name`, y= Deaths, fill= `Cause Name`)) +
+ggplot(bardata_new,aes(x=`Cause.Name`, y= Deaths, fill= `Cause.Name`)) +
   geom_bar(stat = "identity") +
   labs(title = "Top 10 Leading Causes of Deaths in the US in 2017" ,
        x = "Cause of Death",
